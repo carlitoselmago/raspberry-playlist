1a3b2e8f00065ac389dce94bb50389b4e37ecbc7
carlitoselmago <ccarbonell@gmail.com>
2023-01-27 00:33:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ if __name__ == "__main__":
     for i in range(5):
         print("loop ",i)
         #remove finished video
-        RSV.loaded_videos.pop(0)
+        #RSV.loaded_videos.pop(0)
         #play loaded and ready video 
         print("loaded",RSV.loaded_videos)
         RSV.loaded_videos[-1].play()
