a3e5fdd3dbb2f96537660ac0db2b215dfeb82581
carlitoselmago <ccarbonell@gmail.com>
2023-01-27 13:23:22 +0100

    Update main.py

--- main.py
+++ main.py
@@ -81,11 +81,13 @@ for i in range(3,160):
     while shoulwait:
         try:
             stat = RSV.loaded_videos[0].playback_status()
+            print(stat)
             # check stat if you like, maybe reacting to Stopped if user can stop it..
         except :
+            print("ENDED")
             shouldwait=False
             # presumably the video/audio playback ended. do what you need to do..
-            pass
+            
 
 
 
