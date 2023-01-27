d4f0fe50cc8c28f61c351fb96d47384cf049a352
carlitoselmago <ccarbonell@gmail.com>
2023-01-27 11:54:28 +0100

    Update borrame.py

--- borrame.py
+++ borrame.py
@@ -29,10 +29,11 @@ sleep(player2.duration()) #####
 
 player.play()
 player2 = OMXPlayer( videos[2],dbus_name='org.mpris.MediaPlayer2.omxplayer3',args='--no-osd --no-keys -b')
-player2.pause() #####
-sleep(player.duration()-1)
+player2.pause() 
+sleep(player.duration()-2) #####
 
 player2.play()
+sleep(player2.duration()) #####
 """
 for v in videos:
     player = OMXPlayer( v)
