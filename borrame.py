a56927bd3dbaf261e44ccfe5aa5c56104f3f859a
carlitoselmago <ccarbonell@gmail.com>
2023-01-27 12:40:38 +0100

    Update borrame.py

--- borrame.py
+++ borrame.py
@@ -23,22 +23,27 @@ def get_random_video(lastvideo=False):
 
 videos=glob.glob(vidfolder+"/*.mp4")
 
+#primeros dos ready
 player = OMXPlayer( videos[0],dbus_name='org.mpris.MediaPlayer2.omxplayer1',args='--no-osd --no-keys -b')
 player.pause()
 player2 = OMXPlayer( videos[1],dbus_name='org.mpris.MediaPlayer2.omxplayer2',args='--no-osd --no-keys -b')
-player2.pause()
-player2.play()
+#reproduce player2
 sleep(player2.duration()) #####
-start = time.time()
-#player2.quit()
+
+#reproduce player
 player.play()
-player2 = OMXPlayer( videos[2],dbus_name='org.mpris.MediaPlayer2.omxplayer1',args='--no-osd --no-keys -b')
+
+#mientras reproduce player1
+start = time.time()
+player2 = OMXPlayer( videos[2],dbus_name='org.mpris.MediaPlayer2.omxplayer3',args='--no-osd --no-keys -b')
 player2.pause() 
 #player.exitEvent += lambda _, exit_code: player2.play()
+
 end = time.time()
 elapsed=(end-start)
-#print("elapsed",elapsed)
+#reproduce player1
 sleep(player.duration()-elapsed) #####
+
 
 player2.play()
 sleep(player2.duration()) #####
